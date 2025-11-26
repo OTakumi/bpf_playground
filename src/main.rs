03dfb44121e47cde4c86e212f48031c46ea26e2b
OTakumi <takumi.o.0410@gmail.com>
2025-11-26 21:14:18 +0900

    feat: XDPプログラムの初期実装
    aya_ebpfとaya_log_ebpfを用いて、XDPプログラムのエントリポイント
    xdp_helloを追加。パケット受信時にログを出力し、全てのトラフィック
    を許可する基本的な処理を実装。panicハンドラも追加。

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
+use aya_log_ebpf::info;
+
+#[xdp]
+pub fn xdp_hello(ctx: XdpContext) -> u32 {
+    // main entory point
+    match unsafe { try_xdp_hello(ctx) } {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_ABORTED,
+    }
+}
+
+unsafe fn try_xdp_hello(ctx: XdpContext) -> Result<u32, u32> {
+    // Write a log entry every time a packet is received.
+    info!(&ctx, "received a packet");
+
+    // This function returns a Result that permits all traffic.
+    Ok(xdp_action::XDP_PASS)
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop()
+}
